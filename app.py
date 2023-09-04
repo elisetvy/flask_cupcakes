8d76f1c1124f9d0aa5f6314e1e702e1cbbf7f79e
Elise Ngo <elisetvy@gmail.com>
2023-09-04 12:13:23 -0700

    Create Cupcake model and first route

--- app.py
+++ app.py
@@ -1,1 +1,33 @@
 """Flask app for Cupcakes"""
+
+import os
+
+from flask import Flask, render_template, redirect, jsonify
+# from flask_debugtoolbar import DebugToolbarExtension
+
+from models import db, connect_db, Cupcake
+
+app = Flask(__name__)
+
+app.config['SECRET_KEY'] = "secret"
+
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
+    "DATABASE_URL", "postgresql:///cupcakes")
+
+connect_db(app)
+
+# Having the Debug Toolbar show redirects explicitly is often useful;
+# however, if you want to turn it off, you can uncomment this line:
+#
+# app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+
+# toolbar = DebugToolbarExtension(app)
+
+@app.get('/api/cupcakes')
+def get_cupcake_data():
+    """Get data about all cupcakes."""
+
+    cupcakes = Cupcake.query.all()
+    serialized = [cupcake.serialize() for cupcake in cupcakes]
+
+    return jsonify(cupcakes=serialized)
