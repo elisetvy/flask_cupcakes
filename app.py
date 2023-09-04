bd4fda0182e65c79a8f318930cbf9a2968e87ac5
Grace Bethany Hoober <grace.hoober66@gmail.com>
2023-09-04 13:21:58 -0700

    two routes completed

--- app.py
+++ app.py
@@ -2,7 +2,7 @@
 
 import os
 
-from flask import Flask, render_template, redirect, jsonify
+from flask import Flask, render_template, redirect, jsonify, request
 # from flask_debugtoolbar import DebugToolbarExtension
 
 from models import db, connect_db, Cupcake
@@ -24,10 +24,41 @@ connect_db(app)
 # toolbar = DebugToolbarExtension(app)
 
 @app.get('/api/cupcakes')
-def get_cupcake_data():
+def get_all_cupcakes():
     """Get data about all cupcakes."""
 
     cupcakes = Cupcake.query.all()
     serialized = [cupcake.serialize() for cupcake in cupcakes]
 
     return jsonify(cupcakes=serialized)
+
+@app.get('/api/cupcakes/<int:cupcake_id>')
+def get_cupcake(cupcake_id):
+    """Get a cupcake"""
+
+    cupcake = Cupcake.query.get_or_404(cupcake_id)
+    serialized = cupcake.serialize()
+
+    return jsonify(cupcake=serialized)
+
+@app.post('/api/cupcakes')
+def create_cupcake():
+    """Create new cupcake"""
+
+    flavor = request.json["flavor"]
+    size = request.json["size"]
+    rating = request.json["rating"]
+    image_url = request.json["image_url"] or None
+
+    new_cupcake = Cupcake(flavor=flavor,
+                          size=size,
+                          rating=rating,
+                          image_url=image_url)
+
+    db.session.add(new_cupcake)
+    db.session.commit()
+
+    serialized = new_cupcake.serialize()
+
+    return jsonify(cupcake=serialized)
+
