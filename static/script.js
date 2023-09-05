959ff1221f4fd48950c7e9a776eb81f0ef928492
Grace Bethany Hoober <grace.hoober66@gmail.com>
2023-09-04 17:05:26 -0700

    Creating display function and debugging JS requests

--- static/script.js
+++ static/script.js
@@ -1,6 +1,7 @@
 "use strict";
 
-const BASE_API_URL = "https://localhost:5001/cupcakes/";
+const BASE_API_URL = "http://localhost:5001/api/cupcakes";
+const $cupcakeList = $("#cupcake-list");
 
 async function getAllCupcakes() {
   const response = await fetch(BASE_API_URL,
@@ -8,14 +9,26 @@ async function getAllCupcakes() {
     method: 'GET',
     }
   );
-
-    return response.cupcakes; // returns list of cupcake objects
+    const data = await response.json()
+    console.log(response, "response")
+    console.log(data, "data")
+    return data.cupcakes; // returns list of cupcake objects
 }
 
-const cupcakes = await getAllCupcakes();
+const cupcakes = getAllCupcakes();
 
 
 // list all cupcakes
+function displayCupcakes(){
+  //flavor, image, rating, size
+  for(const cupcake of cupcakes){
+    const $cupcake = $(`<li>${cupcake.flavor}
+                        <img width="100px" src=${cupcake.image_url}/>
+                        </li>`)
+
+    $cupcakeList.append($cupcake)
+  }
+}
 // handle form submission and updates the list on page
 // want cupcake name and URL
 
